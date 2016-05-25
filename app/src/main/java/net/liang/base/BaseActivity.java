93276ac1ff1b344f769350fe804d85442aff26a9
梁惠涌 <lianghuiyong@outlook.com>
2016-05-25 19:40:34 +0800

    toolbar第二版

---
+++ app/src/main/java/net/liang/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package net.liang.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lenovo on 2016/5/25.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
--- app/src/main/java/net/liang/ui/MainActivity.java
+++ app/src/main/java/net/liang/ui/MainActivity.java
@@ -1,15 +1,63 @@
 package net.liang.ui;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
 
 import net.liang.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initData();
+        initView();
+
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar_layout);
+        if (appBarLayout!= null){
+            appBarLayout.setExpanded(false);    //
+        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.syncState();
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    private void initView() {
+
+    }
+
+    private void initData() {
+
     }
 }
