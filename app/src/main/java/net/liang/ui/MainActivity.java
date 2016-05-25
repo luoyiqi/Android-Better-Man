35ac953c8d0240779da0d8b49d128efb099a0bb4
梁惠涌 <lianghuiyong@foxmail.com>
2016-05-26 00:02:47 +0800

    tabs

--- app/src/main/java/net/liang/ui/MainActivity.java
+++ app/src/main/java/net/liang/ui/MainActivity.java
@@ -1,19 +1,41 @@
 package net.liang.ui;
 
+import android.graphics.Color;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 import net.liang.R;
+import net.liang.fragment.MainNewsFragment;
+import net.liang.fragment.MainTweetFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentTabHost mTabHost;
+    private ViewPager mViewPager;
+    private List<Fragment> mFragmentList;
+    private Class mClass[] = {MainNewsFragment.class,MainTweetFragment.class};
+    private Fragment mFragment[] = {new MainNewsFragment(),new MainTweetFragment()};
+    private String mTitles[] = {"综合","动弹"};
+    private int mImages[] = {R.drawable.tab_icon_new, R.drawable.tab_icon_tweet};
 
 
 
@@ -24,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         initData();
         initView();
+        initEvent();
 
         AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar_layout);
         if (appBarLayout!= null){
@@ -38,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,8 +78,74 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+
+        mFragmentList = new ArrayList<>();
+
+        mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+        mTabHost.getTabWidget().setDividerDrawable(null);
+
+        for (int i = 0;i < mFragment.length;i++){
+            TabHost.TabSpec tabSpec = mTabHost.newTabSpec(mTitles[i]).setIndicator(getTabView(i));
+            mTabHost.addTab(tabSpec,mClass[i],null);
+            mFragmentList.add(mFragment[i]);
+            mTabHost.getTabWidget().getChildAt(i).setBackgroundColor(Color.WHITE);
+        }
+
+        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return mFragmentList.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mFragmentList.size();
+            }
+        });
+    }
+
+    private View getTabView(int index) {
+        View view = LayoutInflater.from(this).inflate(R.layout.item_main_tab, null);
+
+        ImageView image = (ImageView) view.findViewById(R.id.image);
+        TextView title = (TextView) view.findViewById(R.id.title);
+
+        image.setImageResource(mImages[index]);
+        title.setText(mTitles[index]);
+
+        return view;
+    }
+
+    private void initEvent() {
+
+        mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                mViewPager.setCurrentItem(mTabHost.getCurrentTab());
+            }
+        });
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                mTabHost.setCurrentTab(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
     }
+
 
     private void initData() {
 
